551e12d52a404c29671c77a508bf11050ff89742
redamihradi <redamihradi@hotmail.com>
2022-12-26 12:56:16 +0100

    creation squelette du projet

---
+++ src/main/java/ma/mihradi/projetgpt/ProjetGptApplication.java
@@ -0,0 +1,13 @@
+package ma.mihradi.projetgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetGptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/mihradi/projetgpt/ProjetGptApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.mihradi.projetgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
